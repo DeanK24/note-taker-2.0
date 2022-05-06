aea12ed6698dce79cd435e83845857d5a0f76324
Dean <deank242@gmail.com>
2022-05-06 15:08:24 -0400

    forgot to add routes to server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001
-
+const routeNotes = require('./routes/routesNotes/notesRoutes');
+const routesHtml = require('./routes/htmlRoutes/htmlRoutes');
 const app = express();
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // parse incoming JSON data
 app.use(express.json());
 
-
+app.use('/api', routeNotes);
+app.use('/', routesHtml);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
