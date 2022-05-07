97799da547b4cb37db3b56b9fc2b10dbcb96b10f
Dean Ketchmark <deank242@gmail.com>
2022-05-07 17:43:50 -0400

    changes

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001
-const routeNotes = require('./routes/routesNotes/notesRoutes');
-const routesHtml = require('./routes/htmlRoutes/htmlRoutes');
 const app = express();
 
+const routeNotes = require('./routes/routesNotes/notesRoutes');
+const routesHtml = require('./routes/htmlRoutes/htmlRoutes');
+
 // parse incoming string or array data
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // parse incoming JSON data
-app.use(express.json());
 
 app.use('/api', routeNotes);
 // app.use('/', routesHtml);
